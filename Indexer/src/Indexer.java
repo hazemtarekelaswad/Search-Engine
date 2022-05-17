b89154d3560e49067f98a24b4a9812322259a0bc
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2022-05-17 20:50:45 +0200

    Add scores to the db

--- Indexer/src/Indexer.java
+++ Indexer/src/Indexer.java
@@ -1,18 +1,24 @@
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.util.*;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
 import static java.lang.Math.log10;
 import static java.util.Arrays.asList;
+import static com.mongodb.client.model.Projections.*;
 
 
 public class Indexer {
@@ -29,6 +35,7 @@ public class Indexer {
 
         pagesCount = urls.size();
 
+        // Setup every crawled page
         long threadsIndex = 0;
         Thread[] threads = new Thread[(int) pagesCount];
         for (String url : urls) {
@@ -49,6 +56,8 @@ public class Indexer {
         // TODO: Send these pages to another class in constructor
 //        Ranker ranker = new Ranker(pages);
 
+
+        // Insert every word in each page in the DB
         for (PageInfo page : pages) {
             for (WordInfo word : page.getWords()) {
                 System.out.println(word.getName());
@@ -57,6 +66,8 @@ public class Indexer {
             System.err.println("Number of words: " + page.getWords().size());
         }
 
+        insertScores(collection);
+
         Utility.dbDisconnect();
 
         // listen for any coming crawled url in order ot index it in the db
@@ -64,12 +75,13 @@ public class Indexer {
 
     }
 
-    private static Document createPageDoc(WordInfo word, PageInfo page) {
+    private static Document createPageDoc(WordInfo word, PageInfo page, double idf) {
         return new Document()
                 .append("url", page.getUrl())
                 .append("title", page.getTitle())
                 .append("termFreq", page.getNormTermFreq(word.getName()))
                 .append("advTermFreq", page.getNormAdvTermFreq(word.getName()))
+                .append("score", idf * page.getNormAdvTermFreq(word.getName()))
                 .append("tags", word.getTags())
                 .append("weights", word.getWeights())
                 .append("sentences", word.getSentences());
@@ -79,23 +91,91 @@ public class Indexer {
 
         Document wordDoc = (Document) collection.find(eq("word", word.getName())).first();
 
+        // If the word doesn't exist, add it
         if (wordDoc == null) {
             Document document = new Document()
                     .append("word", word.getName())
                     .append("df", (long) 1)
                     .append("idf", log10((double) pagesCount / 1))
-                    .append("pages", asList(createPageDoc(word, page)));
+                    .append("pages", asList(createPageDoc(word, page, (double) pagesCount / 1)));
             collection.insertOne(document);
-        } else {
+
+            System.err.println("NEW");
+        } else { // you are in a new page, because every pageInfo has unique words
             long docFreq = (long) wordDoc.get("df");
             ++docFreq;
+            double newIdf = log10((double) pagesCount / docFreq);
+
             Bson update = Updates.combine(
-                    Updates.addToSet("pages", createPageDoc(word, page)),
+                    Updates.addToSet("pages", createPageDoc(word, page, newIdf)),
                     Updates.set("df", docFreq),
-                    Updates.set("idf", log10((double) pagesCount / docFreq))
+                    Updates.set("idf", newIdf)
             );
+
             collection.updateOne(wordDoc, update);
+            System.err.println("EXISTS");
+
         }
+    }
+
+    private static void insertScores(MongoCollection collection) {
+
+        List<Document> docs = (List<Document>) collection.find().into(new ArrayList<Document>());
+        for(Document doc : docs) {
+            double idf = doc.getDouble("idf");
+            ArrayList<Document> pages = (ArrayList<Document>) doc.get("pages");
+            for(Document pg : pages) {
+                double advTf = pg.getDouble("advTermFreq");
+                double score = advTf * idf;
+
+                Bson filter = Filters.eq( "_id", doc.get("_id"));
+                Bson pulling = Updates.pull("pages", new Document("url", pg.getString("url")));
+                collection.updateOne(filter, pulling);
+
+                Bson adding = Updates.push("pages", new Document()
+                        .append("url", pg.getString("url"))
+                        .append("title", pg.getString("title"))
+                        .append("termFreq", pg.getDouble("termFreq"))
+                        .append("advTermFreq", pg.getDouble("advTermFreq"))
+                        .append("score", score)
+                        .append("tags", (List<String>) pg.get("tags"))
+                        .append("weights", (List<String>) pg.get("weights"))
+                        .append("sentences", (List<String>) pg.get("sentences"))
+                );
+
+                collection.updateOne(filter, adding);
+            }
+        }
+
+
+//        MongoCursor<Document> wordCursor = collection.find().cursor();
+//
+//        try {
+//            while (wordCursor.hasNext()) {
+//                Document wordDoc = wordCursor.next();
+//                double inverseDocFreq = (double) wordDoc.get("idf");
+//
+//                List<Document> pages = (List<Document>) collection.find().projection(fields(include("pages"))).map(document -> document.get("nodes")).first();
+//                System.out.println(pages.next().get("url"));
+
+//                MongoCollection<Document> pageCursor = collection.find(eq("pages.1.")).cursor();
+////                double termFreq = (double) pageDoc.get("termFreq");
+//                double advTermFreq = (double) pageCursor.next().("advTermFreq");
+//                double score = advTermFreq * inverseDocFreq;
+//                Bson update = Updates.set("score", score);
+//                collection.updateOne(wordDoc.get("pages"), update);
+//
+//                List<Document> pages = (List<Document>) wordDoc.get("pages");
+//                for (Document pageDoc : pages) {
+//
+//
+//
+//                }
+//            }
+//        } finally {
+//            wordCursor.close();
+//        }
+
     }
 
     // Incremental Update: It must be possible to update an existing
@@ -128,7 +208,6 @@ public class Indexer {
         } finally {
             docCursor.close();
         }
-
 
 
         // TODO: or update nothing in case of existing crawled page changes
